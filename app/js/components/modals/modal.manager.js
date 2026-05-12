f8f8bd53e09da42dd43f2af661e5b0424cac2485
omarSanchez <omisp21@gmail.com>
2026-05-12 17:29:25 -0400

    Add modal manager

---
+++ app/js/components/modals/modal.manager.js
@@ -0,0 +1,8 @@
+import { UI } from "../../core/ui";
+
+windows.abrirDetallePersona = function(persona) {
+    const visita = (window.DATOS_FILAS_GLOBALES || []).filter(f => f.persona === nombrePersona);
+
+    
+
+}
